93e134471a1aa68df0b484a8cc8fec0c99a796a1
IM123634\12363 <musashi123busi@gmail.com>
2026-01-19 21:16:59 +0900

    01/19 変更分

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +14,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        \DB::table('users')->insert([
+            [
+                'over_name' => '一二三',
+                'under_name' => '六三四',
+                'over_name_kana' => 'イズミ',
+                'under_name_kana' => 'ムサシ',
+                'mail_address' => 'IMCompass634@gmail.com',
+                'sex' => 1,
+                'birth_day' => '2000-02-01',
+                'role' => 4,
+                'password' => Hash::make('password634'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
